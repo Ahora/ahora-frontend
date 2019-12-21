d54a551ddc37da7ab9e7d0c8baab87abeaba10b2
Avi <aviklaiman@hotmail.com>
2019-12-21 18:31:30 +0200

    Added store to statuses

--- src/app/services/statuses.ts
+++ src/app/services/statuses.ts
@@ -1,38 +1,51 @@
-
 import { RestCollectorClient } from "rest-collector";
+import { store } from "app/store";
 
 export interface Status {
-    id?: number;
-    name: string;
-    description: string;
+  id?: number;
+  name: string;
+  description: string;
 }
 
-const statusesClient: RestCollectorClient = new RestCollectorClient("/api/organizations/{organizationId}/statuses/{id}");
+const statusesClient: RestCollectorClient = new RestCollectorClient(
+  "/api/organizations/{organizationId}/statuses/{id}"
+);
 export const getList = async (): Promise<Status[]> => {
-    const result = await statusesClient.get({
-        params: { organizationId: "ahora" }
-    });
-    return result.data;
-}
+  const result = await statusesClient.get({
+    params: {
+      organizationId: store.getState().organizations.currentOrganization?.login
+    }
+  });
+  return result.data;
+};
 
-export const add = async (organizationId: string, status: Status): Promise<Status> => {
-    const result = await statusesClient.post({
-        params: { organizationId },
-        data: status
-    });
-    return result.data;
-}
+export const add = async (
+  organizationId: string,
+  status: Status
+): Promise<Status> => {
+  const result = await statusesClient.post({
+    params: { organizationId },
+    data: status
+  });
+  return result.data;
+};
 
-export const editStatus = async (organizationId: string, status: Status): Promise<Status> => {
-    const result = await statusesClient.put({
-        params: { organizationId, id: status.id! },
-        data: status
-    });
-    return result.data;
-}
+export const editStatus = async (
+  organizationId: string,
+  status: Status
+): Promise<Status> => {
+  const result = await statusesClient.put({
+    params: { organizationId, id: status.id! },
+    data: status
+  });
+  return result.data;
+};
 
-export const deleteStatus = async (organizationId: string, id: number): Promise<void> => {
-    await statusesClient.delete({
-        params: { organizationId, id }
-    });
-}
+export const deleteStatus = async (
+  organizationId: string,
+  id: number
+): Promise<void> => {
+  await statusesClient.delete({
+    params: { organizationId, id }
+  });
+};

eff73678370e87c16d5d735d29e27b4ba51f8d91
Avi <aviklaiman@hotmail.com>
2019-12-21 18:20:42 +0200

    Removed redundant browserrouter

--- src/app/pages/organizations/details/index.tsx
+++ src/app/pages/organizations/details/index.tsx
@@ -1,105 +1,160 @@
-import * as React from 'react';
-import { RouteComponentProps, Switch, Route } from 'react-router';
-import { Organization, getOrganizationByLogin } from '../../../services/organizations';
-import { BrowserRouter } from 'react-router-dom';
+import * as React from "react";
+import { RouteComponentProps, Switch, Route } from "react-router";
+import {
+  Organization,
+  getOrganizationByLogin
+} from "../../../services/organizations";
 import Nav from "react-bootstrap/Nav";
-import StatusesPage from 'app/pages/statusesPage';
-import DocsPage from 'app/pages/docs';
-import DocsDetailsPage from 'app/pages/docs/details';
-import AddDocPage from 'app/pages/docs/add';
-import EditDocPage from 'app/pages/docs/edit';
-import OrganizationSettingsPage from '../settings';
-import { Dispatch } from 'redux';
-import { setCurrentOrganization } from 'app/store/organizations/actions';
-import { connect } from 'react-redux';
-import { ApplicationState } from 'app/store';
+import StatusesPage from "app/pages/statusesPage";
+import DocsPage from "app/pages/docs";
+import DocsDetailsPage from "app/pages/docs/details";
+import AddDocPage from "app/pages/docs/add";
+import EditDocPage from "app/pages/docs/edit";
+import OrganizationSettingsPage from "../settings";
+import { Dispatch } from "redux";
+import { setCurrentOrganization } from "app/store/organizations/actions";
+import { connect } from "react-redux";
+import { ApplicationState } from "app/store";
 
 interface OrganizationDetailsPageProps {
-    organization: Organization | null;
+  organization: Organization | null;
 }
 
 interface OrganizationPageParams {
-    login: string;
-    section: string
+  login: string;
+  section: string;
 }
-
 
 interface DispatchProps {
-    setOrganizationToState(organization: Organization | null): void;
+  setOrganizationToState(organization: Organization | null): void;
 }
 
-
-interface Props extends RouteComponentProps<OrganizationPageParams>, DispatchProps, OrganizationDetailsPageProps {
-
-}
-
+interface Props
+  extends RouteComponentProps<OrganizationPageParams>,
+    DispatchProps,
+    OrganizationDetailsPageProps {}
 
 class OrganizationDetailsPage extends React.Component<Props> {
+  constructor(props: Props) {
+    super(props);
+  }
 
-    constructor(props: Props) {
-        super(props);
+  async componentDidMount() {
+    const organization: Organization | null = await getOrganizationByLogin(
+      this.props.match.params.login
+    );
+    this.props.setOrganizationToState(organization);
+  }
+  render = () => {
+    const organization = this.props.organization;
+    if (organization) {
+      return (
+        <div>
+          <h2>{organization.login}</h2>
+          <p>{organization.description}</p>
+          <Nav
+            className="mb-3"
+            variant="tabs"
+            defaultActiveKey={this.props.match.params.section || "home"}
+          >
+            <Nav.Item>
+              <Nav.Link
+                eventKey="home"
+                href={`/organizations/${organization.login}`}
+              >
+                Home
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link
+                eventKey="videos"
+                href={`/organizations/${organization.login}/videos`}
+              >
+                Videos
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link
+                eventKey="postmortems"
+                href={`/organizations/${organization.login}/postmortems`}
+              >
+                Postmortems
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link
+                eventKey="discussions"
+                href={`/organizations/${organization.login}/discussions`}
+              >
+                Discussions
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link
+                eventKey="labels"
+                href={`/organizations/${organization.login}/labels`}
+              >
+                Labels
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link
+                eventKey="settings"
+                href={`/organizations/${organization.login}/settings`}
+              >
+                Settings
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+          <Switch>
+            <Route
+              path={`/organizations/:login/settings/:settingsSection?`}
+              component={OrganizationSettingsPage}
+            />
+            <Route
+              path={`/organizations/:login/statuses`}
+              component={StatusesPage}
+            />
+            <Route
+              path={`/organizations/:login/:docType/add`}
+              component={AddDocPage}
+            />
+            <Route
+              path={`/organizations/:login/:docType/:id/edit`}
+              component={EditDocPage}
+            />
+            <Route
+              path={`/organizations/:login/:docType/:id`}
+              component={DocsDetailsPage}
+            />
+            <Route
+              path={`/organizations/:login/:docType`}
+              component={DocsPage}
+            />
+          </Switch>
+        </div>
+      );
+    } else {
+      return <div>Loading....</div>;
     }
-
-    async componentDidMount() {
-        const organization: Organization | null = await getOrganizationByLogin(this.props.match.params.login);
-        this.props.setOrganizationToState(organization);
-    }
-    render = () => {
-        const organization = this.props.organization;
-        if (organization) {
-            return (
-                <div>
-                    <h2>{organization.login}</h2>
-                    <p>{organization.description}</p>
-                    <Nav className="mb-3" variant="tabs" defaultActiveKey={this.props.match.params.section || "home"}>
-                        <Nav.Item>
-                            <Nav.Link eventKey="home" href={`/organizations/${organization.login}`}>Home</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link eventKey="videos" href={`/organizations/${organization.login}/videos`}>Videos</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link eventKey="postmortems" href={`/organizations/${organization.login}/postmortems`}>Postmortems</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link eventKey="discussions" href={`/organizations/${organization.login}/discussions`}>Discussions</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link eventKey="labels" href={`/organizations/${organization.login}/labels`}>Labels</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link eventKey="settings" href={`/organizations/${organization.login}/settings`}>Settings</Nav.Link>
-                        </Nav.Item>
-                    </Nav>
-                    <BrowserRouter>
-                        <Switch>
-                            <Route path={`/organizations/:login/settings/:settingsSection?`} component={OrganizationSettingsPage} />
-                            <Route path={`/organizations/:login/statuses`} component={StatusesPage} />
-                            <Route path={`/organizations/:login/:docType/add`} component={AddDocPage} />
-                            <Route path={`/organizations/:login/:docType/:id/edit`} component={EditDocPage} />
-                            <Route path={`/organizations/:login/:docType/:id`} component={DocsDetailsPage} />
-                            <Route path={`/organizations/:login/:docType`} component={DocsPage} />
-                        </Switch>
-                    </BrowserRouter>
-                </div>
-            );
-        }
-        else {
-            return (<div>Loading....</div>)
-        }
-    };
+  };
 }
 
 const mapStateToProps = (state: ApplicationState) => {
-    return {
-        organization: state.organizations.currentOrganization
-    };
+  return {
+    organization: state.organizations.currentOrganization
+  };
 };
 
 const mapDispatchToProps = (dispatch: Dispatch): DispatchProps => {
-    return {
-        setOrganizationToState: (organization: Organization) => { dispatch(setCurrentOrganization(organization)) },
+  return {
+    setOrganizationToState: (organization: Organization) => {
+      dispatch(setCurrentOrganization(organization));
     }
-}
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(OrganizationDetailsPage as any); 
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(OrganizationDetailsPage as any);
--- src/app/pages/organizations/settings.tsx
+++ src/app/pages/organizations/settings.tsx
@@ -1,76 +1,105 @@
-import * as React from 'react';
-import { RouteComponentProps, Switch, Route } from 'react-router';
-import { BrowserRouter, Link } from 'react-router-dom';
+import * as React from "react";
+import { RouteComponentProps, Switch, Route } from "react-router";
+import { Link } from "react-router-dom";
 import Nav from "react-bootstrap/Nav";
-import StatusesPage from 'app/pages/statusesPage';
-import { Organization, getOrganizations } from 'app/services/organizations';
-import LabelsPage from 'app/pages/labels';
-import PeoplePage from 'app/pages/people';
-
+import StatusesPage from "app/pages/statusesPage";
+import { Organization, getOrganizations } from "app/services/organizations";
+import LabelsPage from "app/pages/labels";
+import PeoplePage from "app/pages/people";
 
 interface SettingsPageState {
-    organization: Organization | null;
+  organization: Organization | null;
 }
 
 interface SettingsPageParams {
-    login: string;
-    section: string;
-    settingsSection: string;
+  login: string;
+  section: string;
+  settingsSection: string;
 }
 
+interface Props extends RouteComponentProps<SettingsPageParams> {}
 
-interface Props extends RouteComponentProps<SettingsPageParams> {
-
-}
-
-
-export default class OrganizationSettingsPage extends React.Component<Props, SettingsPageState> {
-
-    constructor(props: Props) {
-        super(props);
-        this.state = {
-            organization: null
-        };
-    }
-
-    async componentDidMount() {
-        const organizations: Organization[] = await getOrganizations();
-        const organization: Organization = organizations.filter(x => x.login === this.props.match.params.login)[0];
-        this.setState({
-            organization
-        });
-    }
-    render = () => {
-        const organization = this.state.organization;
-        if (organization) {
-            return (
-                <div>
-                    <Nav className="mb-3" variant="tabs" defaultActiveKey={this.props.match.params.settingsSection || "home"}>
-                        <Nav.Item>
-                            <Link className="nav-link" to={`/organizations/${organization.login}/settings`}>Home</Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Link className="nav-link" to={`/organizations/${organization.login}/settings/statuses`}>Statuses</Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link eventKey="labels" href={`/organizations/${organization.login}/settings/labels`}>Labels</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link eventKey="people" href={`/organizations/${organization.login}/settings/people`}>People</Nav.Link>
-                        </Nav.Item>
-                    </Nav>
-                    <BrowserRouter>
-                        <Switch>
-                            <Route path={`/organizations/:login/settings/statuses`} component={StatusesPage} />
-                            <Route path={`/organizations/:login/settings/labels`} component={LabelsPage} />
-                            <Route path={`/organizations/:login/settings/people`} component={PeoplePage} />
-                        </Switch>
-                    </BrowserRouter>
-                </div>
-            );
-        }
-        else {
-            return (<div>Loading....</div>)
-        }
+export default class OrganizationSettingsPage extends React.Component<
+  Props,
+  SettingsPageState
+> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      organization: null
     };
+  }
+
+  async componentDidMount() {
+    const organizations: Organization[] = await getOrganizations();
+    const organization: Organization = organizations.filter(
+      x => x.login === this.props.match.params.login
+    )[0];
+    this.setState({
+      organization
+    });
+  }
+  render = () => {
+    const organization = this.state.organization;
+    if (organization) {
+      return (
+        <div>
+          <Nav
+            className="mb-3"
+            variant="tabs"
+            defaultActiveKey={this.props.match.params.settingsSection || "home"}
+          >
+            <Nav.Item>
+              <Link
+                className="nav-link"
+                to={`/organizations/${organization.login}/settings`}
+              >
+                Home
+              </Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Link
+                className="nav-link"
+                to={`/organizations/${organization.login}/settings/statuses`}
+              >
+                Statuses
+              </Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Link
+                className="nav-link"
+                to={`/organizations/${organization.login}/settings/labels`}
+              >
+                Labels
+              </Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Link
+                className="nav-link"
+                to={`/organizations/${organization.login}/settings/people`}
+              >
+                People
+              </Link>
+            </Nav.Item>
+          </Nav>
+          <Switch>
+            <Route
+              path={`/organizations/:login/settings/statuses`}
+              component={StatusesPage}
+            />
+            <Route
+              path={`/organizations/:login/settings/labels`}
+              component={LabelsPage}
+            />
+            <Route
+              path={`/organizations/:login/settings/people`}
+              component={PeoplePage}
+            />
+          </Switch>
+        </div>
+      );
+    } else {
+      return <div>Loading....</div>;
+    }
+  };
 }

5668d90bbef425da9cf812db5de232167debb1e0
Erez Alster <erez@alster.co.il>
2020-01-26 09:00:15 +0200

    Show pinned comments at the top

--- src/app/components/Comments/List/index.tsx
+++ src/app/components/Comments/List/index.tsx
@@ -11,15 +11,14 @@ interface CommentsProps {
 
 interface State {
     comments?: Comment[];
+    pinnedComments?: Comment[];
 }
 
 export class CommentListComponent extends React.Component<CommentsProps, State> {
     constructor(props: CommentsProps) {
         super(props);
 
-        this.state = {
-            comments: undefined
-        }
+        this.state = {}
     }
 
     onDeleteComment(id: number): void {
@@ -38,13 +37,24 @@ export class CommentListComponent extends React.Component<CommentsProps, State> 
     async componentDidMount() {
         const comments: Comment[] = await getComments(this.props.login, this.props.docId);
         this.setState({
-            comments
+            comments,
+            pinnedComments: comments.filter(comment => comment.pinned)
         });
     }
 
     render() {
         return (
             <div>
+                {this.state.pinnedComments && this.state.pinnedComments.length > 0 &&
+                    (<>
+                        <h3>Pinned Comments</h3>
+                        <div className="list">
+                            {this.state.pinnedComments.map((comment: Comment) => {
+                                return (<CommentDetailsComponent onDelete={this.onDeleteComment.bind(this)} login={this.props.login} key={comment.id} comment={comment}></CommentDetailsComponent>);
+                            })}
+                        </div>
+                    </>)
+                }
                 <div className="mt-2 mb-2">
                     <AddCommentComponent commentAdded={(comment) => { this.commentAdded(comment) }} login={this.props.login} docId={this.props.docId}></AddCommentComponent>
                 </div>

99c4badecb9b475ac82bdec095075de0c1c29b3b
Erez Alster <erez@alster.co.il>
2020-08-12 15:47:18 +0300

    bug fixes - use the right url!

--- src/app/pages/docs/default.tsx
+++ src/app/pages/docs/default.tsx
@@ -1,13 +1,21 @@
 import * as React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, RouteComponentProps } from 'react-router-dom';
 import { Button } from 'antd';
 
-export default class DefaultDocsPage extends React.Component {
+interface PageParams {
+    login: string;
+}
+
+interface Props extends RouteComponentProps<PageParams> {
+
+}
+
+export default class DefaultDocsPage extends React.Component<Props> {
     render() {
         return <div className="defaultdoccontainer">
             <p>Start a discussion or just select your relevant doc!</p>
             <div>
-                <Link to="add">
+                <Link to={`/organizations/${this.props.match.params.login}/docs/add`}>
                     <Button type="primary">Start a new discussion</Button>
                 </Link>
             </div>

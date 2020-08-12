6b2d40663cdca14c9173859d40e3881a351490b5
Erez Alster <erez@alster.co.il>
2020-08-12 15:39:41 +0300

    more cleanup

--- src/main.tsx
+++ src/main.tsx
@@ -10,22 +10,11 @@ import { App } from './app';
 import './general-styles.scss';
 import { store, history } from 'app/store';
 
-const messages_en = require("./translations/en.json");
-const messages_es = require("./translations/es.json");
-const messages: { [id: string]: any } = {
-  'en': messages_en,
-  'es': messages_es
-};
-let language = navigator.language.split(/[-_]/)[0]; // language without region code  
-if (!messages[language]) {
-  language = 'en';
-}
-
 
 // prepare store
 ReactDOM.render(
   <Provider store={store}>
-    <IntlProvider locale={language} messages={messages[language]}>
+    <IntlProvider locale="en">
       <ConnectedRouter history={history} store={store}>
         <App />
       </ConnectedRouter>

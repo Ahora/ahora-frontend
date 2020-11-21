4d0e40777c536330a04eca211cc377e056434327
Erez Alster <ealster@localhost.localdomain>
2020-11-21 08:50:28 +0200

    Support docs WS

--- src/app/websockets/organizationWS.ts
+++ src/app/websockets/organizationWS.ts
@@ -3,6 +3,8 @@ import { store } from 'app/store';
 import { Comment } from "app/services/comments";
 import { deleteCommentInState, setCommentInState } from 'app/store/comments/actions';
 import io from 'socket.io-client';
+import { deleteDocInState, setDocInState } from 'app/store/docs/actions';
+import { Doc } from 'app/services/docs';
 
 export default class OrganizationWebSocket {
 
@@ -11,7 +13,7 @@ export default class OrganizationWebSocket {
     constructor(organizationId: string) {
         const socket = io();
 
-
+        //Comments
         socket.on('comment-post', (comment: Comment) => {
             store.dispatch(setCommentInState(comment));
         });
@@ -24,6 +26,19 @@ export default class OrganizationWebSocket {
             store.dispatch(deleteCommentInState(comment.docId, comment.id));
         });
 
+
+        //Docs
+        socket.on('docs-post', (doc: Doc) => {
+            store.dispatch(setDocInState(doc));
+        });
+
+        socket.on('docs-put', (doc: Doc) => {
+            store.dispatch(setDocInState(doc));
+        });
+
+        socket.on('docs-delete', (doc: Comment) => {
+            store.dispatch(deleteDocInState(doc.id));
+        });
     }
 
     close() {

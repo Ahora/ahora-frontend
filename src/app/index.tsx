85a403bc8ccae0faa6ac777623b4d7a0f6d90617
Erez Alster <ealster@localhost.localdomain>
2021-01-23 11:56:18 +0200

    Allow to swtich local and direction

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,15 +2,40 @@ import * as React from 'react';
 import Dashboard from 'app/containers/Dashboard';
 import { Route, Switch } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
+import { ConfigProvider } from 'antd';
+import { IntlProvider } from 'react-intl';
+import localMap from './../Internationalization';
+import { ApplicationState } from './store';
+import { connect } from 'react-redux';
 
-export class App extends React.Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <Switch>
-          <Route path="/" component={Dashboard} />
-        </Switch>
-      </BrowserRouter>
-    );
-  }
+interface AppProps {
+  messages: any;
+  locale?: string;
+  isRTL: boolean;
 }
+
+function AppComponent(props: AppProps) {
+  return (
+    <ConfigProvider direction={props.isRTL ? "rtl" : "ltr"}>
+      <IntlProvider messages={props.messages} locale={props.locale || "en"} defaultLocale="en">
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" component={Dashboard} />
+          </Switch>
+        </BrowserRouter>
+      </IntlProvider>
+    </ConfigProvider>
+  );
+}
+
+const mapStateToProps = (state: ApplicationState): AppProps | void => {
+  if (state.organizations.currentOrganization) {
+    return {
+      messages: (localMap as any)[state.organizations.currentOrganization.locale || "en"],
+      locale: state.organizations.currentOrganization.locale,
+      isRTL: state.organizations.currentOrganization.isRTL
+    }
+  }
+};
+
+export const App = connect(mapStateToProps)(AppComponent as any) as any; 
--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,17 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { IntlProvider } from 'react-intl';
 import { ConnectedRouter } from 'react-router-redux';
 import { Provider } from 'react-redux';
 import { App } from './app';
-import { ConfigProvider } from 'antd';
 import { store, history } from 'app/store';
-import localMap from './Internationalization';
-
 import './general-styles.scss';
-
-const tolat: any = localMap.he;
 
 // prepare store
 ReactDOM.render(
   <Provider store={store}>
-
-    <ConfigProvider direction="rtl">
-      <IntlProvider messages={tolat} locale="he" defaultLocale="en">
-        <ConnectedRouter history={history} store={store}>
-          <App />
-        </ConnectedRouter>
-      </IntlProvider>
-    </ConfigProvider>
+    <ConnectedRouter history={history} store={store}>
+      <App />
+    </ConnectedRouter>
   </Provider>,
 
   document.getElementById('root')

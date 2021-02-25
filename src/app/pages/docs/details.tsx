19c06718f20e0ff739cb73a25d08d8a32f1efde4
Erez Alster <ealster@localhost.localdomain>
2021-02-25 17:59:51 +0200

    add hotkeys

--- src/app/pages/docs/details.tsx
+++ src/app/pages/docs/details.tsx
@@ -30,6 +30,7 @@ import { addWatcheToDocInState, DeleteWatcheFromDocInState, requestDocToState } 
 import DocTypeText from 'app/components/localization/DocTypeText';
 import { FormattedMessage } from 'react-intl';
 import IsPrivateTag from 'app/components/localization/IsPrivateTag';
+import AhoraHotKey from 'app/components/Basics/AhoraHotKey';
 
 
 interface DocsDetailsPageState {
@@ -166,10 +167,12 @@ class DocsDetailsPage extends React.Component<AllProps, DocsDetailsPageState> {
 
     async delete() {
         if (this.props.doc) {
+            const deletedDoc: Doc = { ...this.props.doc };
+
             if (this.props.onDocDeleted) {
                 this.props.onDocDeleted(this.props.doc);
             }
-            await deleteDoc(this.props.match.params.login, this.props.doc.id!);
+            await deleteDoc(this.props.match.params.login, deletedDoc.id);
         }
     }
 
@@ -243,9 +246,11 @@ class DocsDetailsPage extends React.Component<AllProps, DocsDetailsPageState> {
                                         }
                                         actions={
                                             canEdit ? [
-                                                <Popconfirm onConfirm={this.delete.bind(this)} title="Are you sure?">
-                                                    <span><FormattedMessage id="deleteDocType" /></span>
-                                                </Popconfirm>
+                                                <AhoraHotKey action={this.delete.bind(this)} shortcut="alt+d">
+                                                    <Popconfirm onConfirm={this.delete.bind(this)} title="Are you sure?">
+                                                        <span><FormattedMessage id="deleteDocType" /></span>
+                                                    </Popconfirm>
+                                                </AhoraHotKey>
                                             ] : undefined
                                         }
                                         avatar={
